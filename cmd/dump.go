bb317072cdee13d42f419caff773f5cf9f012fa7
Brandon Smith <brandon.smith@cedana.ai>
2024-05-15 11:04:17 -0500

    fix: rootpath

--- cmd/dump.go
+++ cmd/dump.go
@@ -218,7 +218,7 @@ var dumpRuncCmd = &cobra.Command{
 		}
 
 		dumpArgs := task.RuncDumpArgs{
-			Root:           root,
+			Root:           rootPath[root],
 			CheckpointPath: dir,
 			ContainerID:    id,
 			CriuOpts:       criuOpts,
